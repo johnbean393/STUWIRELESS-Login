44cd8f855b19b26b25a52d756b9983daecbbc9de
johnbean393 <johnbean393@gmail.com>
2025-01-20 18:44:33 +0800

    Improved UI and added missing translations.

--- STUWIRELESS Login/Views/Steps/DoneView.swift
+++ STUWIRELESS Login/Views/Steps/DoneView.swift
@@ -12,9 +12,15 @@ struct DoneView: View {
     var body: some View {
 		VStack {
 			HStack {
-				Text("This app will automatically log you into STUWIRELESS when your Mac wakes from sleep.")
+				Image(systemName: "checkmark.circle.fill")
+					.resizable()
+					.frame(width: 60, height: 60)
+					.foregroundColor(.green)
+					.imageScale(.large)
+				Text("**Setup Complete**")
+					.font(.largeTitle)
+				Text("This app will automatically log you into STUWIRELESS when your Mac wakes from sleep. **You can now close this window.**")
 					.font(.title3)
-					.bold()
 					.multilineTextAlignment(.leading)
 					.fixedSize(horizontal: false, vertical: true)
 				Divider()
